150a802413cc8e466e75daff73e8cfee3e2e6abb
Orlando Fiol <fiolorlando@gmail.com>
2019-04-03 11:51:26 -0400

    Add UnitTest

--- src/tasks/tests.py
+++ src/tasks/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from django_webtest import WebTest
+
+
+class TaskTest(WebTest):
+
+    def test_login(self):
+        # WHEN anonymous user try to access
+        index = self.app.get('/',)
+
+        # THEN sites redirects to login page
+        self.assertEqual(index.status_code, 302)
+
+        # GIVEN a registed user
+        user = get_user_model().objects.create_user(
+                username='testuser',
+                password='12345')
+
+        # WHEN users logged tries to open the site
+        index = self.app.get('/', user=user)
+
+        # THEN access granted
+        self.assertEqual(index.status_code, 200)
